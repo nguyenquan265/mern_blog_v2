ce4d15a4bfcb4961f72d94299ce9a1c19e1c4223
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-07-04 19:59:43 +0700

    page animation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
         <Link to='/' className='flex-none w-10'>
           <img src='/imgs/logo.png' className='w-full' />
         </Link>
+
         {/* Search input */}
         <div
           className={`absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${
@@ -24,6 +25,7 @@ const Navbar = () => {
           />
           <i className='fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey'></i>
         </div>
+
         <div className='flex items-center gap-3 md:gap-6 ml-auto'>
           {/* Open search input under md screen */}
           <button
@@ -32,11 +34,13 @@ const Navbar = () => {
           >
             <i className='fi fi-rr-search text-xl'></i>
           </button>
+
           {/* Editor button */}
           <Link to='/editor' className='hidden md:flex gap-2 link'>
             <i className='fi fi-rr-file-edit'></i>
             <p>Write</p>
           </Link>
+
           {/* User button */}
           <Link to='/signin' className='btn-dark py-2'>
             Sign In
--- client/src/pages/UserAuthForm.jsx
+++ client/src/pages/UserAuthForm.jsx
@@ -1,68 +1,74 @@
 import { Link } from 'react-router-dom'
 import InputBox from '../components/InputBox'
+import PageAnimation from '../common/PageAnimation'
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className='h-cover flex items-center justify-center'>
-      <form className='w-[80%] max-w-[400px]'>
-        <h1 className='text-4xl font-gelasio capitalize text-center mb-16'>
-          {type === 'sign-in' ? 'Welcome Back' : 'Join Us Today'}
-        </h1>
-        {/* Form fields */}
-        {type !== 'sign-in' ? (
+    <PageAnimation keyValue={type}>
+      <section className='h-cover flex items-center justify-center'>
+        <form className='w-[80%] max-w-[400px]'>
+          <h1 className='text-4xl font-gelasio capitalize text-center mb-16'>
+            {type === 'sign-in' ? 'Welcome Back' : 'Join Us Today'}
+          </h1>
+          {/* Form fields */}
+          {type !== 'sign-in' ? (
+            <InputBox
+              name='fullname'
+              type='text'
+              placeholder='Full Name'
+              icon='user'
+            />
+          ) : (
+            ''
+          )}
           <InputBox
-            name='fullname'
-            type='text'
-            placeholder='Full Name'
-            icon='user'
+            name='email'
+            type='email'
+            placeholder='Email'
+            icon='envelope'
           />
-        ) : (
-          ''
-        )}
-        <InputBox
-          name='email'
-          type='email'
-          placeholder='Email'
-          icon='envelope'
-        />
-        <InputBox
-          name='password'
-          type='password'
-          placeholder='Password'
-          icon='key'
-        />
-        {/* Submit button */}
-        <button className='btn-dark center mt-14' type='submit'>
-          {type.replace('-', ' ')}
-        </button>
-        <div className='relative w-full flex items-center gap-2 my-4 opacity-30 uppercase text-black font-bold'>
-          <hr className='w-1/2 border-black' />
-          <p>or</p>
-          <hr className='w-1/2 border-black' />
-        </div>
-        {/* Google button */}
-        <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
-          <img src='/imgs/google.png' className='w-5' />
-          countinue with google
-        </button>
+          <InputBox
+            name='password'
+            type='password'
+            placeholder='Password'
+            icon='key'
+          />
 
-        {type === 'sign-in' ? (
-          <p className='mt-6 text-dark-grey text-xl text-center'>
-            Don't have an account?{' '}
-            <Link to='/signup' className='underline text-black text-xl ml-1'>
-              Sign up
-            </Link>
-          </p>
-        ) : (
-          <p className='mt-6 text-dark-grey text-xl text-center'>
-            Already have an account?{' '}
-            <Link to='/signin' className='underline text-black text-xl ml-1'>
-              Sign In
-            </Link>
-          </p>
-        )}
-      </form>
-    </section>
+          {/* Submit button */}
+          <button className='btn-dark center mt-14' type='submit'>
+            {type.replace('-', ' ')}
+          </button>
+          <div className='relative w-full flex items-center gap-2 my-4 opacity-30 uppercase text-black font-bold'>
+            <hr className='w-1/2 border-black' />
+            <p>or</p>
+            <hr className='w-1/2 border-black' />
+          </div>
+
+          {/* Google button */}
+          <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
+            <img src='/imgs/google.png' className='w-5' />
+            countinue with google
+          </button>
+
+          {/* Navigate between auth page */}
+          {type === 'sign-in' ? (
+            <p className='mt-6 text-dark-grey text-xl text-center'>
+              Don't have an account?{' '}
+              <Link to='/signup' className='underline text-black text-xl ml-1'>
+                Sign up
+              </Link>
+            </p>
+          ) : (
+            <p className='mt-6 text-dark-grey text-xl text-center'>
+              Already have an account?{' '}
+              <Link to='/signin' className='underline text-black text-xl ml-1'>
+                Sign In
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </PageAnimation>
   )
 }
 
