d9c4faeccf6be8cd73655e07521ee6b3a00ec84f
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-07-13 22:13:06 +0700

    set up comment api

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,7 @@ const router = Router()
 import authRouter from './auth.routes'
 import blogRouter from './blog.routes'
 import userRouter from './user.routes'
+import commentRouter from './comment.routes'
 
 router.use('/check', (req, res) => {
   res.status(200).json({ status: 'success', message: 'Server is running' })
@@ -12,6 +13,7 @@ router.use('/check', (req, res) => {
 router.use('/auth', authRouter)
 router.use('/blogs', blogRouter)
 router.use('/users', userRouter)
+router.use('/comments', commentRouter)
 router.all('*', (req, res, next) =>
   next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`))
 )
