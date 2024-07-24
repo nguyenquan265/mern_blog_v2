48a6c87dc082003b1c5db1c5208b5e361c3c968c
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-07-24 10:33:23 +0700

    update for manage blog and draft

--- client/src/components/LoadMoreButton.jsx
+++ client/src/components/LoadMoreButton.jsx
@@ -1,12 +1,26 @@
-const LoadMoreButton = ({ state, fetchFunction, deletedDocCount }) => {
+const LoadMoreButton = ({ state, fetchFunction, deletedDocCount, manage }) => {
+  // If the totalDocs is greater than the number of results, show the load more button
   if (state !== null && state.totalDocs > state.results.length) {
     return (
       <button
         className='text-dark-grey py-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2'
         onClick={() => {
-          if (deletedDocCount) {
-            fetchFunction(parseInt(state.page) + 1, deletedDocCount)
+          // deletedDocCount for notificationPage and ManageBlogsPage
+          // manage for ManageBlogsPage
+          // if manage is not passed, it means it is NotificationPage
+          // if manage is passed, it means it is ManageBlogsPage
+          if (deletedDocCount || manage) {
+            if (manage) {
+              if (manage === 'published') {
+                fetchFunction(parseInt(state.page) + 1, deletedDocCount, false)
+              } else {
+                fetchFunction(parseInt(state.page) + 1, deletedDocCount, true)
+              }
+            } else {
+              fetchFunction(parseInt(state.page) + 1, deletedDocCount)
+            }
           } else {
+            // fetchFunction for other pages
             fetchFunction(parseInt(state.page) + 1)
           }
         }}
